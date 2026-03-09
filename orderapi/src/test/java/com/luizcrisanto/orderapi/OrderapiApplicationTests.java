f042c3266c55bf4c406ff507f34d76ac52fbbf20
luizcrisantoo <luiz.crisanto@gmail.com>
2026-03-09 11:09:07 -0300

    chore: initialize spring boot project

---
+++ orderapi/src/test/java/com/luizcrisanto/orderapi/OrderapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizcrisanto.orderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
